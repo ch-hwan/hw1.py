72b3e064fe9db73d947e41e20660484a1a849fe6
이창환 <changhwanlee06@gmail.com>
2025-04-08 17:33:01 +0900

    Update hw5.py

--- hw5.py
+++ hw5.py
@@ -1,15 +1,21 @@
 def read_single_digit(digit):
     han_digits = ['영', '일', '이', '삼', '사', '오', '육', '칠', '팔', '구']
-    print(han_digits[digit], end=' ')
+    return han_digits[digit]
 
 def read_number(number):
-    result = ''
     han_digits = ['영', '일', '이', '삼', '사', '오', '육', '칠', '팔', '구']
-    for digit_char in str(number):
-        digit = int(digit_char)
-        result += han_digits[digit] + ''
+    
+    hundred = number // 100
+    ten = (number % 100) // 10
+    one = number % 10
+
+    result = read_single_digit(hundred) + ' '
+    result += read_single_digit(ten) + ' '
+    result += read_single_digit(one)
+
     return result.strip()
 
-num = int(input('세 자리 정수 입력'))
+num = int(input('세 자리 정수 입력: '))
 han_string = read_number(num)
 print('한글 문자열:', han_string)
+
