577460da302b906eb4286e0f5e443e55ae991c02
이창환 <changhwanlee06@gmail.com>
2025-04-21 13:57:53 +0900

    Create hw6.py

---
+++ hw6.py
@@ -0,0 +1,10 @@
+for i in range(1, 10):
+    for gugudan in range(2, 6):
+        print(f"{gugudan} x {i:>2} = {gugudan*i:<2}", end="\t")
+    print()
+
+print()  
+for i in range(1, 10):
+    for gugudan in range(6, 10):
+        print(f"{gugudan} x {i:>2} = {gugudan*i:<2}", end="\t")
+    print()
